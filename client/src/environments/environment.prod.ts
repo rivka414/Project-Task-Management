d70edc005dcd461f4eb4c99a66055d3b4d20b1a4
rivka414 <r0534148535@gmail.com>
2026-01-26 16:13:40 +0200

    change wrong api

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiּUrl: 'https://project-task-management-q6vk.onrender.com' // הכתובת שקיבלת מרנדר
+  apiUrl: 'https://project-task-management-q6vk.onrender.com' // הכתובת שקיבלת מרנדר
   
 };
